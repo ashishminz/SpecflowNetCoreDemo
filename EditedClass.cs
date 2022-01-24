593e288489efa2591db716204f252ae638030f9c
Gautham Nayak <gautham.9605@gmail.com>
2022-01-24 20:20:55 +0530

    new Commit

---
+++ EditedClass.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecflowNetCoreDemo
+{
+    internal class EditedClass
+    {
+        [Test]
+        public void tester()
+        {
+            Console.WriteLine("from tester");
+        }
+    }
+}
